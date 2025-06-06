6be05c0525682d3f2839bb9f02315b3adf6da7d3
alamfauzan <gfauzan963@gmail.com>
2025-06-06 17:34:44 +0700

    maaf telat

---
+++ 05_Generics/Jurnal_05/Jurnal_05/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System;
+
+public class SimpleDataBase<T>
+{
+    private List<T> storedData = new List<T>();
+    private List<DateTime> inputDates = new List<DateTime>();
+
+    public void AddNewData(T data)
+    {
+        storedData.Add(data);
+        inputDates.Add(DateTime.UtcNow);
+    }
+
+    public void PrintAllData()
+    {
+        for (int i = 0; i < storedData.Count; i++)
+        {
+            Console.WriteLine($"Data {i + 1} berisi: {storedData[i]}, disimpan pada waktu UTC: {inputDates[i]}");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        SimpleDataBase<double> db = new SimpleDataBase<double>();
+        db.AddNewData(22.0);
+        db.AddNewData(11.0);
+        db.AddNewData(10.0);
+        db.PrintAllData();
+    }
+}
---
+++ 05_Generics/TP_05_2211104031/TP_05_2211104031/Program.cs
@@ -0,0 +1,21 @@
+﻿class DataGeneric<T>
+ {
+     private T data;
+     public DataGeneric(T data)
+     {
+         this.data = data;
+     }
+     public void PrintData()
+     {
+         Console.WriteLine($"Data yang tersimpan adalah: {data}");
+     }
+ }
+ 
+ class Program
+ {
+     static void Main(string[] args)
+     {
+         DataGeneric<string> data = new DataGeneric<string>("2211104005");
+         data.PrintData();
+     }
+ }
